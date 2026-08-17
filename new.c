afabefc111f4172fb171f1db86aac76e7c375ef1
koushikghoshcodes <koushikghosh938@gmail.com>
2026-08-17 21:49:03 +0530

    new c file

---
+++ new.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    float b=10.5;
+    char c='k';
+
+   //single line comments printf("a=%d\n",a);
+   /* printf("b=%f",b);
+    printf("\nc=%c",c);multiple line comments*/
+
+    printf("diff\n");
+    printf("a=%d\nb=%f\nc=%c",a,b,c);
+
+
+}
